2c75c3ae1b794e6cd3b26f86aac9dc9ad0d46c4b
unknown <prevetlunatikam@gmail.com>
2024-05-19 01:45:12 +0300

    first commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import '../src/styles/App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Movies } from "./pages/Movies"
+import { Movie } from "./pages/Movie"
+import { Page404 } from "./pages/Page404"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return(
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Movies/>} />
+            <Route path="movie" element={<Movie/>} />
+            <Route path="*" element={<Page404/>} />
+        </Routes>
+      </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ front/src/pages/Movies.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Movies = () => {
+
+    return (
+        <div>
+            Movies
+        </div>
+    )
+}
