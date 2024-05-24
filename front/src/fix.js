62563303a968708d46ec5306b3f48a0e7054efe3
unknown <prevetlunatikam@gmail.com>
2024-05-24 22:20:15 +0300

    first commit

--- front/src/fix.js
+++ front/src/fix.js
@@ -1,4 +1,4 @@
 export const fix = {
-    link: 'http://localhost:3000',
-    linkserver: 'http://localhost:5000'
+    link: 'http://46.101.102.59:3000',
+    linkserver: 'http://46.101.102.59:5000'
 }
